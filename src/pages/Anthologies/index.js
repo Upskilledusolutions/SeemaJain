5b1f8dbfba5a403582b3eb3967ef885049782573
Pushkargaur10 <gaurpushkar10@gmail.com>
2025-04-15 16:03:26 +0530

    1

--- src/pages/Anthologies/index.js
+++ src/pages/Anthologies/index.js
@@ -13,7 +13,7 @@ export default function index() {
         <div className={styles.heading}>Anthologies</div>
         <div className={styles.flex}>
                 {artdata.map(data=>
-                <div className={styles.card}>
+                <div key={data.id} className={styles.card}>
                     <Image className={styles.heroimg} src={data.url} width={500} height={500} alt='image'/>
                     <div className={styles.info}>
                         <div className={styles.name}>{data.name}</div>
